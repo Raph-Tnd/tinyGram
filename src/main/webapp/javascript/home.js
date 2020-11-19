8d7433dd5698f8b89aa20f255e684606d55e3d03
Luyys <56700560+Luyys@users.noreply.github.com>
2020-11-19 20:34:17 +0100

    CSS completion

--- src/main/webapp/javascript/home.js
+++ src/main/webapp/javascript/home.js
@@ -1,6 +1,6 @@
 var HomePage = {
     view: function (){
-        return m(Home)
+        return [m(Home),m(Header),]
     }
 }
 var Home = {
@@ -10,21 +10,54 @@ var Home = {
     },
     view: function(){
         return m('div',[
-            m('div', {id:'left', class:'vertically-centered'},
-                m('img', {class:'frontImg', src:'./frontImg.jpg', alt:'TinyInsta front page image'}),
+            m('div', {id:'leftContainer'},
+                m('img', {class:'leftFrontImg', src:'./img/frontImg.jpg', alt:'TinyInsta front page image'}),
             ),
-            m('div', {id:'right', class:'vertically-centered'},
-                m('h1', "TinyGram"),
+            m('div', {id:'rightContainer'},
+                m('h1', {class:'rightTitle'}, "TinyGram"),
                 m('div', {id : "sign-in-login"}),
-                m('div', {class:'line'}),
-                m('div', {class:'btnDiv'},[
-                        m(m.route.Link,{href: "/profile/"+emailToUniqueName(Profile.email), class:'btn'},"Profile"),
-                ])
+                //m(m.route.Link,{href: "/profile/"+emailToUniqueName(controller.currentUser.getBasicProfile().getEmail()), class:'btn'},"Profile"),
+                
+                m(m.route.Link,{href: "/profile/tenaudraphael", class: 'buttonSkin', id:'rightButton'},"Profile"),
+                
             ),
-            m('div', {class:'signature'},
+            m('div', {class:'signatureContainer'},
                 m.trust('<p>' +
-                    'Louis NORMAND, Raphaël TENAUD, Sylvain BEAUDOIN, Thibault COLIN  -Web & Cloud-</p>'))
+                    'Louis NORMAND, Raphaël TENAUD, Sylvain BEAUDOIN, Thibault COLIN  -Web & Cloud-</p>')),
+
         ])
+    }
+}
+
+var SearchBar = {
+    body:"",
+    view:function(){
+        return m('form', {class: 'searchBarForm', onsubmit: function(e){
+            controller.redirectTo("/profile/"+SearchBar.body)
+        }},
+        [
+        m('div', m("input[type=textarea]", {
+        	id: 'searchBarTextInput',
+            class:'textInputSkin',
+            placeholder:"Rechercher profil...",
+            oninput: function(e) { SearchBar.body = e.target.value }})),
+        m("button",{class:'buttonSkin', id:'searchBarButton'},"Search"),
+    ])
+    }
+}
+
+
+var Header = {
+    view: function(){
+        return m('div',{id: 'headerContainer'},[
+                  m("button", {class:'buttonSkin', id:'headerDisconnectButton', onclick: function() {
+                                  	controller.disconnectUser();
+                                  }},"Déconnexion"),
+                      m(m.route.Link,{href:"/"},m('img',{id:'headerHomeButton',src:'./img/logo_maison.png'})),
+                    
+                  m('div',{id:'headerInputContainer'},m(SearchBar)),
+                  m(m.route.Link,{href:"/"},m('div',{id:'headerLogo'},'TinyGram')),])
+
     }
 }
 
--- src/main/webapp/javascript/webController.js
+++ src/main/webapp/javascript/webController.js
@@ -7,57 +7,71 @@ function entityToProfile(entity){
 var controller = {
     authInstance : "",
     currentUser: "",
-    loginButton:false,
-    checkUserIsProfile:false,
+    callLoginButton:false,
+    callCheckUserIsProfile:false,
     profileCheck: "",
+    userID: "",
+    callFollow:false,
     setGoogleAuth: function(googleAuth){
-        this.authInstance = googleAuth;
+        controller.authInstance = googleAuth;
         console.log("Calling listener");
         controller.authInstance.currentUser.listen(controller.listenerGoogleUser);
         //if user already signed in, redirect automatically
         if (controller.authInstance.isSignedIn.get()){
             controller.loadGoogleUser();
-            controller.redirectTo("/profile/"+Profile.name);
+            //controller.redirectTo("/profile/"+Profile.name);
         }
+
+
     },
     loadGoogleUser: function(){
         console.log("loadGoogleUser");
-        this.currentUser = this.authInstance.currentUser.get();
-        if (this.currentUser == ""){
+        controller.currentUser = controller.authInstance.currentUser.get();
+        if (controller.currentUser == ""){
             console.log("failed");
-
+            return;
         }
         console.log("success");
-        var profile = this.currentUser.getBasicProfile();
-        var id_token =this.currentUser.getAuthResponse().id_token;
-        Profile.name=emailToUniqueName(profile.getEmail());
-        Profile.email=profile.getEmail();
-        Profile.ID=id_token;
-    },
-    disconnectUser: function (){
-        console.log("disconnectUser");
-        var temp = this.authInstance.currentUser.get();
-        if (temp == null){return;}
-        temp.disconnect();
-        controller.currentUser = "";
-        this.redirectTo("/");
+
+        var id_token = controller.currentUser.getAuthResponse().id_token;
+        /*var profile = controller.currentUser.getBasicProfile();
+        if(Profile.name == ""){
+            Profile.name=emailToUniqueName(profile.getEmail());
+            Profile.email=profile.getEmail();
+        }
+
+         */
+        controller.userID=id_token;
     },
     listenerGoogleUser: function(){
         console.log("listening");
-        if (this.authInstance.isSignedIn.get()){
-            this.loadGoogleUser();
+        if (controller.authInstance.isSignedIn.get()){
+            controller.loadGoogleUser();
             console.log("Call redirect");
             Profile.createProfile();
-            this.redirectTo("/profile/"+Profile.name);
+            //controller.redirectTo("/profile/"+Profile.name);
         }
         else{
-
+            console.log(this);
         }
+    },
+    disconnectUser: function (){
+        console.log("disconnectUser");
+        var temp = controller.authInstance.currentUser.get();
+        if (temp == null){return;}
+        temp.disconnect();
+        controller.currentUser = "";
+        controller.redirectTo("/");
     },
     redirectTo: function(route){
         //wait for Profile.name to be updated when logging in before redirecting to User's Timeline
         console.log("redirecting to "+route);
         m.route.set(route);
+    },
+    searchProfile: function(profileName){
+        console.log("Searching for "+profileName);
+        Profile.loadProfile(profileName);
+        controller.redirectTo("/profile/"+profileName);
     }
 }
 
@@ -73,22 +87,19 @@ var Profile = {
         if(Profile.name ==""){
             Profile.loadProfile(vnode.attrs.name);
         }
-        if(this.userIsProfile){
-            return m('div', {class:'container'}, [
-                m("h1", {class: 'title'}, Profile.name),
+        if(Profile.userIsProfile){
+            return m('div', {class:'profileContainer'}, [
+                m("h1", {class: 'profileName'}, Profile.name),
                 m("img",{class: "profilePicture", "src":Profile.url}),
                 //m("button",{class:"button", onclick: function(e) { Profile.loadList()}},"Msgs"),
-                m("button", {class:'buttonSkin', id:'disconnectButton', onclick: function() {
-                	controller.disconnectUser();
-                }},"Déconnexion"),
-                m("div", {class: 'tile'}, m('div',{class:'postForm'},m(PostForm))),
+                m(PostForm),
                 m("div",m(PostView,{profile: Profile}))
             ])
         }else{
-            return m('div', {class:'container'}, [
-                m("h1", {class: 'title'}, Profile.name),
+            return m('div', {class:'profileContainer'}, [
+                m("h1", {class: 'profileName'}, Profile.name),
                 m("img",{class: "profilePicture", "src":Profile.url}),
-                m("button",{class:"button", onclick: function(e) { Profile.loadList()}},"Msgs"),
+                m("button",{class:"buttonSkin", id: 'profileFollow', onclick: function(e) { Profile.follow()}},"Follow"),
                 m("div",m(PostView,{profile: Profile}))
             ])
         }
@@ -97,7 +108,7 @@ var Profile = {
     loadList: function() {
         return m.request({
             method: "GET",
-            url: "_ah/api/myApi/v1/profile/"+Profile.name+"/post"+'?access_token=' + encodeURIComponent(Profile.ID)
+            url: "_ah/api/myApi/v1/profile/"+Profile.name+"/post"+'?access_token=' + encodeURIComponent(controller.userID)
         })
         .then(function(result) {
             console.log("load_list:",result)
@@ -117,12 +128,17 @@ var Profile = {
             url: "_ah/api/myApi/v1/profile/"+Profile.name+"/post",
             params: {
                 'next':Profile.nextToken,
-                'access_token': encodeURIComponent(Profile.ID)
+                'access_token': encodeURIComponent(controller.userID)
             }
         })
         .then(function(result) {
             console.log("next:",result)
-            result.items.map(function(item){Profile.list.push(item)})
+            if (result.items != null){
+                result.items.map(function(item){Profile.list.push(item)})
+            }else{
+                console.log("No post to show");
+            }
+
             if ('nextPageToken' in result) {
                 Profile.nextToken= result.nextPageToken
             } else {
@@ -135,7 +151,7 @@ var Profile = {
             'body': desc}
         return m.request({
             method: "POST",
-            url: "_ah/api/myApi/v1/postMsg"+'?access_token='+encodeURIComponent(Profile.ID),
+            url: "_ah/api/myApi/v1/postMsg"+'?access_token='+encodeURIComponent(controller.userID),
             params: data,
         })
         .then(function(result) {
@@ -149,7 +165,7 @@ var Profile = {
     deleteMessage: function(name) {
         return m.request({
             method: "DELETE",
-            url: "_ah/api/myApi/v1/deleteMsg/"+name+'?access_token='+encodeURIComponent(Profile.ID)
+            url: "_ah/api/myApi/v1/deleteMsg/"+name+'?access_token='+encodeURIComponent(controller.userID)
         })
         .then(function(result) {
             console.log("delete:",result)
@@ -162,7 +178,7 @@ var Profile = {
     createProfile: function(){
         return m.request({
             method: "POST",
-            url: "_ah/api/myApi/v1/profile/create"+'?access_token='+encodeURIComponent(Profile.ID),
+            url: "_ah/api/myApi/v1/profile/create"+'?access_token='+encodeURIComponent(controller.userID),
         })
         .then(function(result){
             console.log(result)
@@ -175,18 +191,18 @@ var Profile = {
     loadProfile: function(profileName){
         console.log(controller.authInstance);
         if(controller.authInstance == ""){
-            controller.checkUserIsProfile = true;
+            controller.callCheckUserIsProfile = true;
             controller.profileCheck = profileName;
             return;
         }
         if(controller.authInstance.isSignedIn.get()){
             if(profileName == emailToUniqueName(controller.currentUser.getBasicProfile().getEmail())){
-                this.userIsProfile = true;
+                Profile.userIsProfile = true;
             }else{
-                this.userIsProfile = false;
+                Profile.userIsProfile = false;
             }
         }
-        this.getProfile(profileName);
+        Profile.getProfile(profileName);
 
     },
     getProfile: function(profileName){
@@ -202,34 +218,50 @@ var Profile = {
             console.log("message: ",e.messages,"code: ", e.code)
         })
     },
-
-
+    follow: function(){
+        console.log("Call follow");
+        if(controller.authInstance == ""){
+            callFollow = true;
+            return;
+        }
+        return m.request({
+            method: "POST",
+            url: "_ah/api/myApi/v1/profile/"+Profile.name+"/follow"+'?access_token='+encodeURIComponent(controller.userID),
+        })
+        .then(function(result){
+            console.log("Followed"+result.properties.name);
+        })
+        .catch(function(e){
+            console.log(e.messages);
+        })
+    },
 }
+
 var PostForm = {
     url:"",
     body:"",
     view: function() {
         return m("form", {
+        	class:'formContainer',
             onsubmit: function(e) {
                 if (this.url =="") {return ;}
                 Profile.postMessage(PostForm.body,PostForm.url)
             }
         },
         [
-        m('div', {class:'field'},[
-        m("label", {class:'label'},"URL de l'image"),
+        m('div',[
         m('div',{class:'control'}, m("input[type=text]", {
-        class:'input',
-        placeholder:"URL",
-        oninput: function(e) {PostForm.url = e.target.value}})),
-        //		          m("img",{"src":this.url}),
+        	class: 'textInputSkin',
+        	id:'formInput',
+        	placeholder:"URL",
+        	oninput: function(e) {PostForm.url = e.target.value}})),
         ]),
         m('div',{class:'field'},[
-        m("label", {class: 'label'},"Description"),
         m('div',{class:'control'},m("input[type=textarea]", {
-        class:'input',
-        placeholder:"Description",
-        oninput: function(e) { PostForm.body = e.target.value }})),
+        	class: 'textInputSkin',
+        	id:'formInput',
+        	placeholder:"Description",
+        	oninput: function(e) { PostForm.body = e.target.value }})),
         ]),
         m('div',{class:'control'},m("button[type=submit]", {class:'buttonSkin', id:'postButton'},"Publier")),
         ])
@@ -242,39 +274,42 @@ var PostView = {
             vnode.attrs.profile.list.map(function(item) {
                 if (vnode.attrs.profile.userIsProfile){
                     return m('div', {class:'postContainer'}, [
-                        m('div', {class: 'likeDiv'},
-                            m('a.link[href=#]', {class:'likeButton', onclick: function(e) { }},"like"),
-                            m('label', {class: 'likeCounter'}, item.properties.likec + " j'aimes"),
-                        ),
-                        m('button', {class:'delButton', onclick: function(e) {
+                        m('button', {class:'postDeleteButton', onclick: function(e) {
                                     Profile.deleteMessage(item.key.name)
                                 }},
-                            m('img', {src:"img/trashIcon.png", class:'trashImg'}),
+                            m('img', {class:'postDeleteButtonImage', src:"img/trashIcon.png"}),
                         ),
-                        m('div', {class: 'bodyDiv'},
-                            m('label', {class: 'bodyPost'}, item.properties.body),
+                        m('div', {class: 'postContainer'},
+                            m('label', {class: 'postBodyContainer'}, item.properties.body),
                         ),
-                        m('img', {class: 'imagePost', 'src': item.properties.url}),
+                        m('img', {class: 'postImage', 'src': item.properties.url}),
+
+                        m('div', {class: 'postLikeContainer'},
+                            m('a.link[href=#]', {class:'postLikeButton', onclick: function(e) { }},
+                            		m('img', {class: 'postLikeButtonImage', src:"img/like.png"})
+                            ),
+                            m('label', {class: 'postLikeCounter'}, item.properties.likec),
+                        ),
                     ])
                 }else{
                     return m('div', {class:'postContainer'}, [
-                        m('div', {class: 'likeDiv'},
-                            m('a.link[href=#]', {class:'likeButton', onclick: function(e) { }},"like"),
-                            m('label', {class: 'likeCounter'}, item.properties.likec + " j'aimes"),
+                        m('div', {class: 'postBodyContainer'},
+                            m('label', {class: 'postBody'}, item.properties.body),
                         ),
-                        m('div', {class: 'bodyDiv'},
-                            m('label', {class: 'bodyPost'}, item.properties.body),
-                        ),
-                        m('img', {class: 'imagePost', 'src': item.properties.url}),
+                        m('img', {class: 'postImage', 'src': item.properties.url}),
+                        m('div', {class: 'postLikeContainer'},
+                                m('a.link[href=#]', {class:'buttonSkin', onclick: function(e) { }},
+                                		m('img', {class: 'postLikeButtonImage', src:"img/like.png"})
+                                ),
+                                m('label', {class: 'postLikeCounter'}, item.properties.likec + " j'aimes"),
+                            ),
                     ])
                 }
-
             }),
             m("input", {
-
                 type: "image",
                 src: "/img/nextArrow.png",
-                class: "nextButton",
+                class: "postNextButton",
                 onclick: function(e) {
                     vnode.attrs.profile.next()
                 },
@@ -283,17 +318,106 @@ var PostView = {
     }
 }
 
+var SearchBar = {
+    body:"",
+    view:function(){
+        return m('form', {class:'searchBarContainer', onsubmit: function(e){
+            controller.searchProfile(SearchBar.body);
+        }},
+        [
+        m('div',{class:'searchBarForm'},[
+        m("input[type=textarea]", {class:'textInputSkin', id: 'searchBarTextInput',
+            placeholder:"Recherche un profile",
+            oninput: function(e) { SearchBar.body = e.target.value }}),
+        ]),
+        m("button[type=submit]", {class:'buttonSkin', id:'searchBarPostButton'},"Go !"),
+    ])
+    }
+}
 var PageProfile = {
     view: function(vnode){
-        return m(Profile, {name: vnode.attrs.user})
+        return [m(Header),m(Profile, {name: vnode.attrs.user})]
     }
+}
+
+var FriendsList = {
+	view: function() {
+		return m(FriendsListView)
+	}
 }
 
 var TimeLine = {
+    nextToken:"",
     list: [],
     view: function(){
+        return m('div', [
+            m('div',{class:'subtitle'}),
+            TimeLine.list.map(function(item) {
+                return m('div', {class:'postContainer'}, [
+                    m('div', {class: 'likeDiv'},
+                        m('a.link[href=#]', {class:'likeButton', onclick: function(e) { }},"like"),
+                        m('label', {class: 'likeCounter'}, item.properties.likec + " j'aimes"),
+                    ),
+                    m('div', {class: 'bodyDiv'},
+                        m('label', {class: 'bodyPost'}, item.properties.body),
+                    ),
+                    m('img', {class: 'imagePost', 'src': item.properties.url}),
+                ])
+            }),
+            m("input", {
+                type: "image",
+                src: "/img/nextArrow.png",
+                class: "nextButton",
+                onclick: function(e) {
+                    TimeLine.getTimeline()
+                },
+            }),
+        ])
+    },
+    loadTimeline: function() {
+        return m.request({
+            method: "GET",
+            url: "_ah/api/myApi/v1/timeline/"+'?access_token=' + encodeURIComponent(controller.userID)
+        })
+            .then(function(result) {
+                console.log("load_list:",result)
+                TimeLine.list=result.items
+                if ('nextPageToken' in result) {
+                    TimeLine.nextToken= result.nextPageToken
+                } else {
+                    TimeLine.nextToken=""
+                }})
+            .catch(function(e){
+                console.log(e)
+            })
+    },
+    getTimeline: function(){
+        console.log("Call getTimeline");
+        return m.request({
+            method: "GET",
+            url: "_ah/api/myApi/v1/timeline",
+            params: {
+                'next':TimeLine.nextToken,
+                'access_token': encodeURIComponent(controller.userID)
+            }
+        })
+        .then(function(result){
+            console.log("next:",result)
+            if (result.items != null){
+                result.items.map(function(item){TimeLine.list.push(item)})
+            }else{
+                console.log("No post to show");
+            }
 
+            if ('nextPageToken' in result) {
+                TimeLine.nextToken= result.nextPageToken
+            } else {
+                TimeLine.nextToken=""
+            }
+        })
     }
 }
+
+
 
 
